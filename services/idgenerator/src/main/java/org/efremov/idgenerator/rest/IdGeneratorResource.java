65fcbf6a1685264d6b52b04ed2ae1542f1607e14
Aleksandr Efremov <alex.efremov.1989@gmail.com>
2017-12-01 10:32:16 +0300

    Remove hazelcast. Add consul library.

--- services/idgenerator/src/main/java/org/efremov/idgenerator/rest/IdGeneratorResource.java
+++ services/idgenerator/src/main/java/org/efremov/idgenerator/rest/IdGeneratorResource.java
@@ -7,8 +7,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import org.efremov.idgenerator.ejb.IDGenerator;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -23,11 +21,8 @@ import org.efremov.idgenerator.ejb.IDGenerator;
 @Produces(MediaType.APPLICATION_JSON)
 public class IdGeneratorResource {
     
-    @Inject
-    IDGenerator idGenerator;
-    
     @GET
     public Response getNewId() {
-        return Response.ok().entity(idGenerator.getId()).build();
+        return Response.ok().entity("").build();
     }
 }
